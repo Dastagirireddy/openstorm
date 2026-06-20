678959b6742f3d4a0b4c62f2eeee21157ddfdfdb
Dasta <dasta@Mac.lan>
2026-06-20 23:59:45 +0530

    Replaced statubar background with activity bar background color

--- src/components/layout/status-bar.ts
+++ src/components/layout/status-bar.ts
@@ -189,7 +189,7 @@ export class StatusBar extends TailwindElement() {
     const lineEnding = 'LF';
 
     return html`
-      <footer class="flex h-[28px] items-center justify-between px-3 text-[12px] bg-[var(--statusbar-background)] text-[var(--statusbar-foreground)] border-t border-[var(--statusbar-border)] select-none">
+      <footer class="flex h-[28px] items-center justify-between px-3 text-[12px] bg-[var(--activitybar-background)] text-[var(--statusbar-foreground)] border-t border-[var(--statusbar-border)] select-none">
 
         <!-- Left: Git, Console -->
         <div class="flex items-center gap-1">
